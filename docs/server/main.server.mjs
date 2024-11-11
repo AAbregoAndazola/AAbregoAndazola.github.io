d0e1a288936b188a3511e2720ebf947e807537af
AlanAbrego <alanabrego235@gmail.com>
2024-11-11 13:57:09 -0600

    Rebuild for GitHub Pages

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-NNKPF4WP.mjs";import"./chunk-PAS5ZVDR.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-2IHMIJ5M.mjs";import"./chunk-PAS5ZVDR.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -11,6 +11,7 @@ import { CommonModule } from '@angular/common';  // Importa CommonModule
 export class ExperienceComponent {
   experiences = [
     { title: 'ALBA Synchrotron', description: 'Developed an approximation model for calculating the properties of a quadrupole doublet, integrated into advanced testing systems.', date: 'March 2023 - August 2025' },
+    { title: 'GRUPO ARGA', description: 'I developed process automation scripts to optimize workflow efficiency. I created an executable Python program that converts PDF documents into images. These images, representing design blueprints, are analyzed by a neural network built from scratch to detect measurements and key element positions. This approach streamlined data extraction and improved accuracy in interpreting technical specifications.', date: 'September 2024 - Present'},
     { title: 'BTSSC', description: 'Developed an interactive dashboard for project management in the merchandising department, using C# and Angular, with the implementation of authentication systems and real-time data visualization.', date: 'February 2024 - August 2024' },
     { title: 'Professional Service in CNC', description: 'Assisted in the repair and maintenance of machinery, focusing on electronic and mechanical systems.', date: 'July 2019 - 2022'}
   ];
