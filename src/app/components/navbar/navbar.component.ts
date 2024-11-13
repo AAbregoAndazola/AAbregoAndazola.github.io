aae100c7241630f8b99fbb1fc7eca3d500ea0fde
AlanAbrego <alanabrego235@gmail.com>
2024-11-13 08:48:07 -0600

    Upgrade

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent {}
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,29 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, Routes } from '@angular/router';
 import { importProvidersFrom } from '@angular/core';
-import { NavbarComponent } from './app/components/navbar/navbar.component';
+import { AppComponent } from './app/app.component'; // Asegúrate de tener un AppComponent como raíz
 import { AboutComponent } from './app/components/about/about.component';
 import { PortafolioComponent } from './app/components/portafolio/portafolio.component';
 import { ExperienceComponent } from './app/components/experience/experience.component';
 import { EducationComponent } from './app/components/education/education.component';
 import { ContactComponent } from './app/components/contact/contact.component';
-
+import { LucideAngularModule, Camera, Moon, Sun } from 'lucide-angular';
+import {  Linkedin, Github, Twitter } from 'lucide-angular';
 const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'portfolio', component: PortafolioComponent },
   { path: 'experience', component: ExperienceComponent },
   { path: 'education', component: EducationComponent },
   { path: 'contact', component: ContactComponent },
-  { path: '', redirectTo: 'about', pathMatch: 'full' }, // Ruta principal redirige a About
-  { path: '**', redirectTo: 'about' } // Redirige rutas no encontradas a About
+  { path: '', redirectTo: 'about', pathMatch: 'full' },
+  { path: '**', redirectTo: 'about' }
 ];
 
-bootstrapApplication(NavbarComponent, {
+bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes)
+    provideRouter(routes),
+    importProvidersFrom(
+      LucideAngularModule.pick({ Camera, Moon, Sun }) // Importa solo los iconos que necesitas
+    )
   ]
 }).catch(err => console.error(err));
